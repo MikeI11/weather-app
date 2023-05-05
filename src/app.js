e02410f4dac3fb93fa0f949365cf32571886bf15
MikeI11 <132368319+MikeI11@users.noreply.github.com>
2023-05-05 22:54:01 +0300

    implemented the unit conversion

--- src/app.js
+++ src/app.js
@@ -25,7 +25,9 @@ function displayTemperature(response) {
     let iconElement = document.querySelector("#icon");
 
 
-    temperatureElement.innerHTML = Math.round(response.data.main.temp);
+    celsiusTemperature = response.data.main.temp;
+
+    temperatureElement.innerHTML = Math.round(celsiusTemperature);
     cityElement.innerHTML = response.data.name;
     descriptionElement.innerHTML = response.data.weather[0].description;
     humidityElement.innerHTML = response.data.main.humidity;
@@ -51,9 +53,36 @@ function handleSubmit(event) {
     
 }
 
+function displayFahrenheitTemperature(event) {
+    event.preventDefault()
+    let temperatureElement = document.querySelector("#temperature");
+    // remove the active class the celsius link
+    celsiusLink.classList.remove("active");
+    fahrenheitLink.classList.add("active");
+    let fahrenheitTemperature = (celsiusTemperature * 9) / 5 + 32;
+    temperatureElement.innerHTML = Math.round(fahrenheitTemperature); 
+}
 
-search("New York")
+function displayCelsiusTemperature(event) {
+    event.preventDefault();
+    celsiusLink.classList.add("active");
+    fahrenheitLink.classList.remove("active");
+    let temperatureElement = document.querySelector("#temperature");
+    temperatureElement.innerHTML = Math.round(celsiusTemperature);
+
+}
+
+let celsiusTemperature = null;
 
 
 let form = document.querySelector("#search-form");
 form.addEventListener("submit", handleSubmit);
+
+let fahrenheitLink = document.querySelector("#fahrenheit-link");
+fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
+
+let celsiusLink = document.querySelector("#celsius-link");
+celsiusLink.addEventListener("click", displayCelsiusTemperature);
+
+
+search("New York")
